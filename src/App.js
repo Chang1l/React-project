4e1e32b4560838c8256d4f5e17cab945f8a7ed44
Chang1l <81501866+Chang1l@users.noreply.github.com>
2021-09-07 20:38:30 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./test.jpg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, ButtonToolbar } from "react-bootstrap";
 
 function App() {
+  let title = "수명 테스트";
+  let share = "공유하기";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>{title}</div>
+        <div class="container">
+          <h1>{title}</h1>
+          <img src={logo} classnName="photo" />
+          <p>
+            수명 테스트
+            <br />
+            내 수명은 얼마나 남았을까?
+            <br />
+            아래 버튼을 눌러 시작해보세요
+            <br />
+          </p>
+
+          <ButtonToolbar>
+            <Button variant="outline-danger" size="lg">
+              테스트 시작
+            </Button>
+          </ButtonToolbar>
+
+          <p>{share}</p>
+        </div>
+      </div>
     </div>
   );
 }
